955e193f44af3fee4780e978023e03bb2be02b3e
Nicholas Buscemi <nicholasbuscemi@Nicholass-MBP.fios-router.home>
2024-06-24 16:09:31 -0400

    update

--- src/_root/pages/AboutUs.tsx
+++ src/_root/pages/AboutUs.tsx
@@ -2,43 +2,48 @@ import About from "@/components/sections/About";
 import About3 from "@/components/sections/About3"
 import Hero3 from "@/components/shared/Hero3"
 import { cloudFlareImages } from "@/data/cloudFlareImagesConfig";
-//import { useSanityContent } from '../../hooks/useSanityContent';  
-//import { ABOUT_QUERY, ABOUT3_QUERY } from '../../queries/sanityQueries';
+import { useSanityContent } from '../../hooks/useSanityContent';  
+import { GENERIC_ABOUT_QUERY } from '../../queries/sanityQueries';
 
 
 const AboutUs = () => {
+
+  const { data: about3Content, error: about3Error, loading: about3Loading } = useSanityContent(GENERIC_ABOUT_QUERY('aboutAnna'));
+  const { data: aboutContent, error: aboutError, loading: aboutLoading } = useSanityContent(GENERIC_ABOUT_QUERY('aboutSammy'));
+  
 
   const h1 = "Anna and Sammy";
   const p = "A gourmet chef and a fine dining service expert, both with their decades of knowledge come together to give you a one of a kind experience.";
   const mediaType = 'image';
   const mediaSrc = cloudFlareImages.aboutheader1;
 
-  const about3Content = {
-    sectionId: 'about',
-    title: "Anna",
-    paragraphs: [
-      "Welcome to GOURMOLOGY, where we specialize in eclectic food that entices every type of palate with our mix of Italian, Spanish, and American contemporary cuisine.",
-      "We bring the restaurant experience to the comfort of your own home. Our private dining experience includes everything you need to have an unforgettable evening."
-    ],
-    imageSrc: cloudFlareImages.fish,
-    imageAlt: 'A delicious dish of fish'
-  };
+  if (about3Error || aboutError ) {
+    return <div>Failed to load content. Please try again later.</div>;
+  }
 
-  const aboutContent = {
-    sectionId: 'about',
-    title: "Sammy",
-    paragraphs: [
-      "Any occasion. Big or small. We help create extraordinary dinners, social gatherings, and celebratory events by placing a strong emphasis on personalized customer service, in addition to a diverse menu packed with crisp flavors & fresh ingredients. Ensuring you and your guests will be raving for weeks to come!"
-    ],
-    imageSrc: cloudFlareImages.lamb,
-    imageAlt: 'A delicious dish of lamb'
-  };
+  if (about3Loading || aboutLoading) {
+    return <Hero3 h1={h1} p={p} mediaType={mediaType} mediaSrc={mediaSrc} />;
+  }
 
   return (
     <>
     <Hero3 h1={h1} p={p} mediaType={mediaType} mediaSrc={mediaSrc} />
-    <About3 {...about3Content} />
-    <About {...aboutContent} />
+    <About3 
+        title={about3Content.title}
+        paragraphs={about3Content.paragraphs}
+        buttonText={about3Content.buttonText}
+        buttonLink={about3Content.buttonLink}
+        imageSrc={about3Content.imageSrc}
+        imageAlt={about3Content.imageAlt} 
+    />
+    <About 
+        title={aboutContent.title}
+        paragraphs={aboutContent.paragraphs}
+        buttonText={aboutContent.buttonText}
+        buttonLink={aboutContent.buttonLink}
+        imageSrc={aboutContent.imageSrc}
+        imageAlt={aboutContent.imageAlt}
+    />
     </>
   )
 }
